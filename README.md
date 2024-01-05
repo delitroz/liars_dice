a584ccd3733b7bba07f09ba64c0432e291859078
delitroz <mdelitroz@gmail.com>
2024-01-05 22:19:39 +0100

    Improve prints

--- README.md
+++ README.md
@@ -2,6 +2,11 @@
 Having some fun with real-time webcam feed processing in python by implementing
 a game of liar's dice where the user plays their dice on a webcam feed.
 
+## TODO
+
+- Re-check the rules on a bid: is it still valid if it is more that what was last bid?
+- Implement players turn. Last round's looser begins next round
+
 
 ## Installation
 
--- liars_dice.py
+++ liars_dice.py
@@ -3,9 +3,13 @@ from src.gameplay import ask_player_name, ask_nb_cpus, Game
 
 if __name__ == "__main__":
 
-    player_name = ask_player_name()
-    nb_cpus = ask_nb_cpus()
-    nb_dices = 5
+    #player_name = ask_player_name()
+    player_name = "Player"
+
+    #nb_cpus = ask_nb_cpus()
+    nb_cpus = 4
+
+    nb_dices = 3
 
     game = Game(player_name, nb_cpus, nb_dices)
     game.play()
